827352f016d6d952013f8895335fde34ba9d38fb
Zhongpeng Lin <zplin@uber.com>
2025-01-02 16:23:23 +0000

    reading fix content

--- go/tools/builders/nogo_validation.go
+++ go/tools/builders/nogo_validation.go
@@ -8,9 +8,10 @@ import (
 func nogoValidation(args []string) error {
 	validationOutput := args[0]
 	logFile := args[1]
+	fixFile := args[2]
 	// Always create the output file and only fail if the log file is non-empty to
 	// avoid an "action failed to create outputs" error.
-	logContent, err := os.ReadFile(logFile);
+	logContent, err := os.ReadFile(logFile)
 	if err != nil {
 		return err
 	}
@@ -18,6 +19,12 @@ func nogoValidation(args []string) error {
 	if err != nil {
 		return err
 	}
+
+	_, err = os.ReadFile(fixFile)
+	if err != nil {
+		return err
+	}
+
 	if len(logContent) > 0 {
 		// Separate nogo output from Bazel's --sandbox_debug message via an
 		// empty line.
